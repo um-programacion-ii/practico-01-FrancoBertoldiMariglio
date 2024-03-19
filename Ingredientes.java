180e9a220157e885a64848e4bcf812cef3364ac6
Franco Bertoldi Mariglio <franco.m.bertoldi@sandinas.com.ar>
2024-03-19 18:58:29 -0300

    agregado de constructor

--- Ingredientes.java
+++ Ingredientes.java
@@ -2,6 +2,11 @@ public class Ingredientes {
     private String nombre;
     private int cantidad;
 
+    public Ingredientes(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
     public String getNombre() {
         return nombre;
     }
