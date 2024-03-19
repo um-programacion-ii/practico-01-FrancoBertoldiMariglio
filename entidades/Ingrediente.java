0dfa06a8cfa62d09d699b13b54ac6fd0eeae7268
Franco Bertoldi Mariglio <franco.m.bertoldi@sandinas.com.ar>
2024-03-19 18:55:56 -0300

    todavia no terminado

--- entidades/Ingrediente.java
+++ entidades/Ingrediente.java
@@ -4,6 +4,11 @@ public class Ingrediente {
     private String nombre;
     private int cantidad;
 
+    public Ingrediente(String nombre, int cantidad) {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        }
+
     public String getNombre() {
         return nombre;
     }
--- entidades/Receta.java
+++ entidades/Receta.java
@@ -1,4 +1,38 @@
 package entidades;
 
 public class Receta {
+    protected int tiempoCoccion;
+    protected Ingrediente[] ingredientes;
+
+    protected String preparacion;
+
+    public Receta(int tiempoCoccion, Ingrediente[] ingredientes, String preparacion) {
+        this.tiempoCoccion = tiempoCoccion;
+        this.ingredientes = ingredientes;
+        this.preparacion = preparacion;
+    }
+
+    public int getTiempoCoccion() {
+        return tiempoCoccion;
+    }
+
+    public Ingrediente[] getIngredientes() {
+        return ingredientes;
+    }
+
+    public String getPreparacion() {
+        return preparacion;
+    }
+
+    public void setTiempoCoccion(int tiempoCoccion) {
+        this.tiempoCoccion = tiempoCoccion;
+    }
+
+    public void setIngredientes(Ingrediente[] ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public void setPreparacion(String preparacion) {
+        this.preparacion = preparacion;
+    }
 }
